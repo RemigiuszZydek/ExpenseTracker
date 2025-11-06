f1bc8f656cbb769cfc1772de6c837258d9afa7cd
remo <zyduremik@gmail.com>
2025-11-06 16:55:36 +0100

    zmienilem w routerze trasy

--- backend/routers/expense_router.py
+++ backend/routers/expense_router.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from backend.database.database import engine, Base, SessionLocal, get_db_session
 from backend.database.models.expense_model import ExpenseModel
 from backend.database.schemas.expense_schema import ExpenseCreate, ExpenseRead
+from backend.services.expense_service import ExpenseService
 
 # Create a new APIRouter instance with endpoint prefix /expenses
 router = APIRouter(
@@ -17,16 +18,8 @@ router = APIRouter(
 
 @router.post("/",response_model=ExpenseRead)
 def create_expense(expense: ExpenseCreate, db: Session=Depends(get_db_session)):
-    # Create a new instance of the ExpenseModel using data from the request
-    db_expense = ExpenseModel(**expense.dict())
-     # Add the new expense to the session (staging it for insertion into the database)
-    db.add(db_expense)
-    # Commit the session to save changes into the database
-    db.commit()
-    # Refresh the object to get updated fields (like auto-generated id and created_at)
-    db.refresh(db_expense)
-    # Return the newly created expense as response
-    return db_expense
+    expense_service = ExpenseService(db)
+    return expense_service.create_expense(expense)
 
 @router.get("/",response_model=list[ExpenseRead])
 def get_expense(db: Session=Depends(get_db_session)):
@@ -39,26 +32,14 @@ def get_expense(db: Session=Depends(get_db_session)):
 def delete_expense(expense_id: int, db: Session=Depends(get_db_session)):
     # Delete expense by id
     db_expense = db.query(ExpenseModel).filter(ExpenseModel.id == expense_id).first()
-    
     if not db_expense:
         raise HTTPException(status_code=404, detail="Expense not found")
-    
     db.delete(db_expense)
     db.commit()
-
     return {"message": f"Expense with id {expense_id} deleted successfully"}
     
 
 @router.put("/{expense_id}",response_model=ExpenseRead)
 def update_expense(expense_id: int, updated_expense: ExpenseCreate, db: Session = Depends(get_db_session)):
-    db_expense = db.query(ExpenseModel).filter(ExpenseModel.id == expense_id).first()
-    # Find expense by ID
-    if not db_expense:
-        raise HTTPException(status_code=404, detail="Expense not found")
-    # Update each field
-    for key,value in updated_expense.dict().items():
-        setattr(db_expense,key,value)
-
-    db.commit()
-    db.refresh(db_expense)
-    return db_expense
+    expense_service = ExpenseService(db)
+    return expense_service.update_expense(expense_id, updated_expense)
